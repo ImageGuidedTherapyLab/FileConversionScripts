9aba99b2ee137b7001a5f24933c83ad23aa6a88b
David Fuentes <fuentesdt@gmail.com>
2013-03-05 10:31:54 -0600

    recursively parse all file from exported OsiriX database

--- DicomSeriesReadImageWrite.py
+++ DicomSeriesReadImageWrite.py
@@ -20,6 +20,7 @@
 #  Example on the use of Dicom Reader 
 #
 
+import os
 import sys
 # python itk bindings
 import itk
@@ -36,53 +37,80 @@ if len(sys.argv) < 1:
 #
 ImageType  = itk.Image.SS3
 
-nameGenerator = itk.GDCMSeriesFileNames.New()
-nameGenerator.SetUseSeriesDetails( True ) 
-nameGenerator.RecursiveOn() 
-nameGenerator.AddSeriesRestriction("0008|0021") 
+for dirname, dirnames, filenames in os.walk(sys.argv[1]):
+    # print path to all subdirectories first.
+    for subdirname in dirnames:
+        PathToSubDir = os.path.join(dirname, subdirname)
+        print "working on: ",PathToSubDir  
+        nameGenerator = itk.GDCMSeriesFileNames.New()
+        nameGenerator.SetUseSeriesDetails( True ) 
+        # os.walk will recursively look through directories
+        nameGenerator.RecursiveOff() 
+        nameGenerator.AddSeriesRestriction("0008|0021") 
+        
+        nameGenerator.SetDirectory( PathToSubDir ) 
+        seriesUID = nameGenerator.GetSeriesUIDs() 
+        for uid in seriesUID:
+           # get file names
+           fileNames = nameGenerator.GetFileNames( uid ) 
 
-nameGenerator.SetDirectory( sys.argv[1]) 
-seriesUID = nameGenerator.GetSeriesUIDs() 
+           try:
+             # read
+             reader = itk.ImageSeriesReader[ImageType].New()
+             dicomIO = itk.GDCMImageIO.New()
+             reader.SetImageIO( dicomIO )
+             reader.SetFileNames( fileNames )
+             reader.Update( )
+             # get dictionary info
+             dictionary = dicomIO.GetMetaDataDictionary()
+             PrintAllKeysInDictionary = False
+             if(PrintAllKeysInDictionary): 
+               for key in dictionary.GetKeys():
+                 print key, dictionary[key]
+             # parse header SeriesDescription for t1 t2 flair
+             WriteThisUID = False
+             SeriesDescription = dictionary['0008|103e']
+             StudyDate         = dictionary['0008|0020']
+             SeriesDate        = dictionary['0008|0021']
+             AcquisitionDate   = dictionary['0008|0022']
+             ContentDate       = dictionary['0008|0023']
+             StudyTime         = dictionary['0008|0030']
+             for searchheader in ['T1','T2','FLAIR']:
+              if(SeriesDescription.upper().find(searchheader) != -1):
+                WriteThisUID = True
+             # write
+             if(WriteThisUID):
+               ## str.isalnum is used to remove special characters:
+               ## 
+               ## S.isalnum() -> bool
+               ## 
+               ## Return True if all characters in S are alphanumeric
+               ## and there is at least one character in S, False otherwise.
+               ##
+               ##
+               ## >>> string = "Special $#! characters   spaces 888323"
+               ## >>> ''.join(e for e in string if e.isalnum())
+               ## 'Specialcharactersspaces888323'
 
-for uid in seriesUID:
-   # get file names
-   fileNames = nameGenerator.GetFileNames( uid ) 
-   print "reading:", uid
-   # read
-   reader = itk.ImageSeriesReader[ImageType].New()
-   dicomIO = itk.GDCMImageIO.New()
-   reader.SetImageIO( dicomIO )
-   reader.SetFileNames( fileNames )
-   reader.Update( )
-   # get dictionary info
-   dictionary = dicomIO.GetMetaDataDictionary()
-   PrintAllKeysInDictionary = False
-   if(PrintAllKeysInDictionary): 
-     for key in dictionary.GetKeys():
-       print key, dictionary[key]
-   # parse header SeriesDescription for t1 t2 flair
-   WriteThisUID = False
-   SeriesDescription = dictionary['0008|103e']
-   StudyDate         = dictionary['0008|0020']
-   SeriesDate        = dictionary['0008|0021']
-   AcquisitionDate   = dictionary['0008|0022']
-   ContentDate       = dictionary['0008|0023']
-   StudyTime         = dictionary['0008|0030']
-   for searchheader in ['T1','T2','FLAIR']:
-    if(SeriesDescription.upper().find(searchheader) != -1):
-      WriteThisUID = True
-      print "writing:", SeriesDescription
-   # write
-   if(WriteThisUID):
-     # tag file name with dicom header info to id
-     filename = "%d_%d_%s" %(StudyDate,StudyTime,SeriesDescription.replace(' ',''))
-     # instantiate writer
-     writer = itk.ImageFileWriter[ImageType].New()
-     writer.SetInput( reader.GetOutput() )
-     #TODO set vtk array name to the series description for ID
-     #vtkvectorarray.SetName(SeriesDescription)
-     writer.SetFileName( "%s.mha" % filename );
-     writer.Update() 
-     #TODO get pixel buffer and save as MATLAB :)
-     #scipyio.savemat("%s.mat" % (filename), {'ImageData':Data,'HeaderInfo':dictionary} )
-     scipyio.savemat("%s.mat" % (filename), {'HeaderInfo':dictionary.GetKeys()} )
+               # tag file name with dicom header info to id
+               outfilename = "%s_%s_%s_%s" %(PathToSubDir,StudyDate,StudyTime,\
+                      ''.join(e for e in SeriesDescription if e.isalnum())
+                                      )
+               print "writing:", outfilename
+               # instantiate writer
+               writer = itk.ImageFileWriter[ImageType].New()
+               writer.SetInput( reader.GetOutput() )
+               #TODO set vtk array name to the series description for ID
+               #vtkvectorarray.SetName(SeriesDescription)
+               writer.SetFileName( "%s.mha" % outfilename );
+               writer.Update() 
+               #TODO get pixel buffer and save as MATLAB :)
+               #scipyio.savemat("%s.mat" % (outfilename), {'ImageData':Data,'HeaderInfo':dictionary} )
+               scipyio.savemat("%s.mat" % (outfilename), {'HeaderInfo':dictionary.GetKeys()} )
+           except Exception as inst:
+             print "error reading: ", uid 
+             print inst
+
+    ## # print path to all filenames.
+    ## for filename in filenames:
+    ##     print os.path.join(dirname, filename)
