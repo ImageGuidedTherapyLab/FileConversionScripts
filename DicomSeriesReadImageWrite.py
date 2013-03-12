5f94aa537716803b6dba86a0eef077a26b65e000
David Fuentes <fuentesdt@gmail.com>
2013-03-11 21:46:25 -0500

    server setup

--- DicomSeriesReadImageWrite.py
+++ DicomSeriesReadImageWrite.py
@@ -23,7 +23,9 @@
 import os
 import sys
 import subprocess
-# python itk bindings
+## parser
+import ConfigParser
+## python itk bindings
 import itk
 # python vtk bindings
 import vtk
@@ -160,97 +162,117 @@ def ConvertVTKMatlab(input_filename,output_filename,headerinfo):
 
 ####################################################################
 
-## if len(sys.argv) < 1:
-##     print('Usage: ' + sys.argv[0] + ' DicomDirectory')
-##     sys.exit(1)
-#
-# Reads a 3D image in with signed short (16bits/pixel) pixel type
-# and save it
-#
-ImageType  = itk.Image.SS3
+def ParseDicomDirectoryAndWrite(DicomDirectory):
+   print "working on: ",DicomDirectory  
+   ImageType  = itk.Image.SS3
+   nameGenerator = itk.GDCMSeriesFileNames.New()
+   nameGenerator.SetUseSeriesDetails( True ) 
+   # os.walk will recursively look through directories
+   nameGenerator.RecursiveOff() 
+   nameGenerator.AddSeriesRestriction("0008|0021") 
+   
+   nameGenerator.SetDirectory( DicomDirectory ) 
+   seriesUID = nameGenerator.GetSeriesUIDs() 
+   for uid in seriesUID:
+      # get file names
+      fileNames = nameGenerator.GetFileNames( uid ) 
 
-AETitle = 'INNOVADOR'
-portnumber = 11112
-listenerCMD = "/opt/apps/SLICER/Slicer-4.1.1-linux-amd64/bin/storescp -xs -fe .dcm -sp  -aet %s --output-directory /home/fuentes/SlicerDicom/ --exec-on-reception 'echo #f 1>&2' --exec-on-eostudy 'echo  #p' --eostudy-timeout 5 %d" % (AETitle,portnumber) 
-print "starting..."
-print listenerCMD 
-listenerProcess = subprocess.Popen(listenerCMD,shell=True,stdout=subprocess.PIPE )
-while ( listenerProcess.poll() == None ):
-## for dirname, dirnames, filenames in os.walk(sys.argv[1]):
-##     # print path to all subdirectories first.
-##     for subdirname in dirnames:
-##         PathToSubDir = os.path.join(dirname, subdirname)
-##         
-        PathToSubDir = listenerProcess.stdout.readline().strip('\n')
-        print "working on: ",PathToSubDir  
-        nameGenerator = itk.GDCMSeriesFileNames.New()
-        nameGenerator.SetUseSeriesDetails( True ) 
-        # os.walk will recursively look through directories
-        nameGenerator.RecursiveOff() 
-        nameGenerator.AddSeriesRestriction("0008|0021") 
-        
-        nameGenerator.SetDirectory( PathToSubDir ) 
-        seriesUID = nameGenerator.GetSeriesUIDs() 
-        for uid in seriesUID:
-           # get file names
-           fileNames = nameGenerator.GetFileNames( uid ) 
+      try:
+        # read
+        reader = itk.ImageSeriesReader[ImageType].New()
+        dicomIO = itk.GDCMImageIO.New()
+        reader.SetImageIO( dicomIO )
+        reader.SetFileNames( fileNames )
+        reader.Update( )
+        # get dictionary info
+        dictionary = dicomIO.GetMetaDataDictionary()
+        PrintAllKeysInDictionary = False
+        if(PrintAllKeysInDictionary): 
+          for key in dictionary.GetKeys():
+            print key, dictionary[key]
+        # parse header SeriesDescription for t1 t2 flair
+        WriteThisUID = False
+        SeriesDescription = dictionary['0008|103e']
+        StudyDate         = dictionary['0008|0020']
+        SeriesDate        = dictionary['0008|0021']
+        AcquisitionDate   = dictionary['0008|0022']
+        ContentDate       = dictionary['0008|0023']
+        StudyTime         = dictionary['0008|0030']
+        for searchheader in ['T1','T2','FLAIR']:
+         if(SeriesDescription.upper().find(searchheader) != -1):
+           WriteThisUID = True
+        # write
+        if(WriteThisUID):
+          ## str.isalnum is used to remove special characters:
+          ## 
+          ## S.isalnum() -> bool
+          ## 
+          ## Return True if all characters in S are alphanumeric
+          ## and there is at least one character in S, False otherwise.
+          ##
+          ##
+          ## >>> string = "Special $#! characters   spaces 888323"
+          ## >>> ''.join(e for e in string if e.isalnum())
+          ## 'Specialcharactersspaces888323'
 
-           try:
-             # read
-             reader = itk.ImageSeriesReader[ImageType].New()
-             dicomIO = itk.GDCMImageIO.New()
-             reader.SetImageIO( dicomIO )
-             reader.SetFileNames( fileNames )
-             reader.Update( )
-             # get dictionary info
-             dictionary = dicomIO.GetMetaDataDictionary()
-             PrintAllKeysInDictionary = False
-             if(PrintAllKeysInDictionary): 
-               for key in dictionary.GetKeys():
-                 print key, dictionary[key]
-             # parse header SeriesDescription for t1 t2 flair
-             WriteThisUID = False
-             SeriesDescription = dictionary['0008|103e']
-             StudyDate         = dictionary['0008|0020']
-             SeriesDate        = dictionary['0008|0021']
-             AcquisitionDate   = dictionary['0008|0022']
-             ContentDate       = dictionary['0008|0023']
-             StudyTime         = dictionary['0008|0030']
-             for searchheader in ['T1','T2','FLAIR']:
-              if(SeriesDescription.upper().find(searchheader) != -1):
-                WriteThisUID = True
-             # write
-             if(WriteThisUID):
-               ## str.isalnum is used to remove special characters:
-               ## 
-               ## S.isalnum() -> bool
-               ## 
-               ## Return True if all characters in S are alphanumeric
-               ## and there is at least one character in S, False otherwise.
-               ##
-               ##
-               ## >>> string = "Special $#! characters   spaces 888323"
-               ## >>> ''.join(e for e in string if e.isalnum())
-               ## 'Specialcharactersspaces888323'
+          # tag file name with dicom header info to id
+          outfilename = "%s_%s_%s_%s" %(DicomDirectory,StudyDate,StudyTime.replace(' ',''),\
+                 ''.join(e for e in SeriesDescription if e.isalnum())
+                                 )
+          print "writing:", outfilename
+          # instantiate writer
+          writer = itk.ImageFileWriter[ImageType].New()
+          writer.SetInput( reader.GetOutput() )
+          #TODO set vtk array name to the series description for ID
+          #vtkvectorarray.SetName(SeriesDescription)
+          writer.SetFileName( "%s.vtk" % outfilename );
+          writer.Update() 
+          #get pixel buffer and save as MATLAB :)
+          ConvertVTKMatlab( "%s.vtk" % (outfilename),"%s.mat" % (outfilename),dictionary.GetKeys() )
+      except Exception as inst:
+        print "error reading: ", uid 
+        print inst
 
-               # tag file name with dicom header info to id
-               outfilename = "%s_%s_%s_%s" %(PathToSubDir,StudyDate,StudyTime.replace(' ',''),\
-                      ''.join(e for e in SeriesDescription if e.isalnum())
-                                      )
-               print "writing:", outfilename
-               # instantiate writer
-               writer = itk.ImageFileWriter[ImageType].New()
-               writer.SetInput( reader.GetOutput() )
-               #TODO set vtk array name to the series description for ID
-               #vtkvectorarray.SetName(SeriesDescription)
-               writer.SetFileName( "%s.vtk" % outfilename );
-               writer.Update() 
-               #get pixel buffer and save as MATLAB :)
-               ConvertVTKMatlab( "%s.vtk" % (outfilename),"%s.mat" % (outfilename),dictionary.GetKeys() )
-           except Exception as inst:
-             print "error reading: ", uid 
-             print inst
-
-    ## # print path to all filenames.
-    ## for filename in filenames:
-    ##     print os.path.join(dirname, filename)
+# setup command line parser to control execution
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option( "--dicom_dir",
+                  action="store", dest="dicom_dir", default=None,
+                  help="recursively convert .vtk and .vti files in this directory to .mat files", metavar = "DIR")
+parser.add_option( "--server",
+                  action="store", dest="server", default=None,
+                  help="setup server with ini file", metavar = "INI FILE")
+(options, args) = parser.parse_args()
+if (options.dicom_dir):
+  for dirname, dirnames, filenames in os.walk(options.dicom_dir):
+    # print path to all subdirectories first.
+    if (len(dirnames)):
+        for subdirname in dirnames:
+            PathToSubDir = os.path.join(dirname, subdirname)
+            ParseDicomDirectoryAndWrite(PathToSubDir)
+        ## # print path to all filenames.
+        ## for filename in filenames:
+        ##     print os.path.join(dirname, filename)
+    # no directories to recurse
+    else: 
+        PathToSubDir = os.path.join(dirname, subdirname)
+        ParseDicomDirectoryAndWrite(PathToSubDir)
+elif (options.server):
+  # configure server
+  config = ConfigParser.ConfigParser()
+  storescpEXE = config.get('dcmtk','storescp')
+  AETitle     = config.get('dcmtk','aetitle')
+  outputdir   = config.get('dcmtk','outputdir')
+  portnumber  = config.getint('dcmtk','port')
+  # build server command
+  listenerCMD = "%s -xs -fe .dcm -sp  -aet %s --output-directory %s  --exec-on-reception 'echo #f 1>&2' --exec-on-eostudy 'echo  #p' --eostudy-timeout 5 %d" % (storescpEXE,AETitle,outputdir,portnumber) 
+  print "starting..."
+  print listenerCMD 
+  # setup server
+  listenerProcess = subprocess.Popen(listenerCMD,shell=True,stdout=subprocess.PIPE )
+  while ( listenerProcess.poll() == None ):
+          PathToSubDir = listenerProcess.stdout.readline().strip('\n')
+          ParseDicomDirectoryAndWrite(PathToSubDir)
+else:
+  parser.print_help()
+  print options
